29919c9a90e6a0c3fa9cc527ae3df52a2caf6e9f
Leo <leoslegos1@gmail.com>
2024-01-08 20:58:45 -0800

    delete old 2+0

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTest.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTest.java
@@ -14,16 +14,18 @@ public class MeepMeepTest {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(52.5, 52.5, 142.9, Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(15, 60+3, Math.toRadians(270)))
-                                .splineToLinearHeading(new Pose2d(24, 39, Math.toRadians(180)), Math.toRadians(270))
-                                .forward(6)
+                        drive.trajectorySequenceBuilder(new Pose2d(15 - 54, 60+3, Math.toRadians(270)))
+                                .splineToLinearHeading(new Pose2d(16 - 54, 36, Math.toRadians(0)), Math.toRadians(225)) // used to be 225
                                 .UNSTABLE_addTemporalMarkerOffset(0, () -> {
                                 })
                                 .UNSTABLE_addTemporalMarkerOffset(1, () -> {
                                 })
                                 .waitSeconds(1)
                                 .back(6)
-                                .splineToSplineHeading(new Pose2d(48, 36, Math.toRadians(180)), Math.toRadians(0))
+                                .strafeLeft(20)
+                                .splineToLinearHeading(new Pose2d(-30, 58, Math.toRadians(180)), Math.toRadians(0))
+                                .setTangent(0)
+                                .splineToLinearHeading(new Pose2d(50, 40, Math.toRadians(180)), Math.toRadians(-45))
                                 .build()
                 );
 
